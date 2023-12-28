4b34f4c5a1a2b2eb7a63615ac7e9c15e3c9c80bc
DESKTOP-HR98PIA\Siti Hajar Soleha <amishbah12@gmail.com>
2023-12-28 14:02:13 +0700

    CRUD User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const NotFoundError = require('../exeptions/NotFoundError');
+const ClientError = require('../exeptions/ClientError');
 const { database } = require('../database');
 
 const router = express.Router();
@@ -12,6 +14,111 @@ router.get('/users', asyncHandler(async (req, res) => {
     data: {
       users,
     },
+  });
+}));
+
+router.get('/users/:id', asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const user = await database('users').where({ id }).first();
+
+  if (!user) {
+    throw new NotFoundError('User tidak ditemukan');
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+}));
+
+router.post('/users', asyncHandler(async (req, res) => {
+  const {
+    name, nik, address, phone,
+  } = req.body;
+
+  const newUser = await database('users').insert({
+    name,
+    nik,
+    address,
+    phone,
+  });
+
+  if (!newUser) {
+    throw new ClientError('Terjadi kesalahan dalam memasukan data');
+  }
+
+  const createdUser = await database('users').where({ id: newUser[0] }).first();
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: createdUser,
+    },
+  });
+}));
+
+router.patch('/users/:id', asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const {
+    username, alamat, password, phone, email,  ktp, picUrl
+  } = req.body;
+
+  const existingUser = await database('users').where({ id }).first();
+
+  if (!existingUser) {
+    throw new NotFoundError('User tidak ditemukan');
+  }
+
+  const dataToSend = {
+    id,
+    username,
+    alamat,
+    password,
+    phone,
+    email,
+    ktp,
+    picUrl,
+  };
+
+  await axios.post('https://waterpositive.my.id/api/UserProfile/UpdateData', dataToSend);
+
+  await database('users').where({ id }).update({
+    username,
+    password,
+    phone,
+    email,
+    alamat,
+    ktp,
+    picUrl,
+  });
+
+  const updatedUser = await database('users').where({ id }).first();
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+}));
+
+router.delete('/users/:id', asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const user = await database('users').where({ id }).first();
+
+  if (!user) {
+    throw new NotFoundError('User tidak ditemukan');
+  }
+
+  await axios.post('https://waterpositive.my.id/api/UserProfile/DeleteData', { id });
+
+  await database('users').where({ id }).del();
+
+  res.status(200).json({
+    status: 'success',
+    message: 'User deleted successfully',
   });
 }));
 
