a2bf5d0f287732373abf0b02600d923765a1232e
DESKTOP-HR98PIA\Siti Hajar Soleha <amishbah12@gmail.com>
2024-01-11 10:18:54 +0700

    user update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,25 +11,6 @@ const { authCheck } = require('../middlewares/auth');
 
 const router = express.Router();
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'src/public');
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
-
-// const upload = multer({
-//   storage: storage,
-//   fileFilter: function (req, file, cb) {
-//     if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-//       return cb(new ClientError('Hanya gambar dengan format jpg, jpeg, png, dan gif yang diizinkan!'));
-//     }
-//     cb(null, true);
-//   }
-// });
-
 router.get('/users', authCheck, asyncHandler(async (req, res) => {
 
   let users;
